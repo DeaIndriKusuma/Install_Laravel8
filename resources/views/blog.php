15085407d53ed20043889674898ae26ff6f5b634
Dea Indri Kusuma <deaindrik@gmail.com>
2022-05-11 20:49:16 +0700

    komit pertama

---
+++ resources/views/blog.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tutorial Laravel - www.deaindrikusuma.com</title>
+</head>
+<body>
+ 
+	<h3>www.deaindrikusuma.com</h3>
+	<p>Nama : Dea Indri Kusuma</p>
+	<p>NIM : G.211.19.0056</p>
+	<p>Praktikum 3_Rekayasa Web</p>
+ 
+</body>
+</html>
